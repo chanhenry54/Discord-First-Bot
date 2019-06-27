d402ec34ace133a8b0a8965f0e02471590b43f62
chanhenry54 <hechan@ucsd.edu>
2019-06-27 01:48:16 -0700

    Added starting framework for !hechan commands

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// startup declarations
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -7,14 +8,60 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+    // ignore bot messages
+    if (msg.author.bot) { return; }
+
     // 'ping' interaction
     if (msg.content.toLowerCase() === 'ping') {
         msg.channel.send('Pong!');
     }
+
     // 'no u' interaction
-    //if (msg.content.toLowerCase() === 'no u') {
-    //  msg.channel.send('No u');
-    //}
+    if (msg.content.toLowerCase() === 'no u') {
+        msg.channel.send('No u!');
+    }
+
+    // 'owo' interaction
+    if (msg.content.toLowerCase() === 'owo') {
+        msg.channel.send('uwu');
+    }
+
+    // 'uwu' interaction
+    if (msg.content.toLowerCase() === 'uwu') {
+        msg.channel.send('owo');
+    }
+
+    /*
+     * !hechan commands
+     */
+    if (!msg.content.startsWith(process.env.PREFIX)) { return; }
+    const args = msg.content.slice(PREFIX.length).trim().split(/ + /g);
+    const command = args.shift().toLowerCase();
+
+    switch (command) {
+        // help command
+        case 'help':
+            msg.author.send({
+                embed: {
+                    "color": 8838087,
+                    "fields": [
+                        {
+                            "name": "Hechan Commands",
+                            "value": "Here is a list of all available Hechan commands and parameters."
+                        },
+                        {
+                            "name": "!hechan help",
+                            "value": "Displays this help guide"
+                        }
+                    ]
+                }
+            });
+            break;
+        // unknown command
+        default:
+            msg.author.send(msg + ': invalid command!');
+            break;
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
