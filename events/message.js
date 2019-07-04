bdc69b6149d0923ac823450657c5fa4b29c8bc47
chanhenry54 <hechan@ucsd.edu>
2019-07-03 19:26:50 -0700

    Changed indexOf to startsWith

--- events/message.js
+++ events/message.js
@@ -28,14 +28,12 @@ module.exports = (client, msg) => {
     }
 
     // Parse command from msg
-    if (msg.content.indexOf(client.config.PREFIX) !== 0) {
+    if (!msg.content.startsWith(client.config.PREFIX)) {
         msg.channel.send('oops');
         return;
     }
     const args = msg.content.slice(client.config.PREFIX.length).trim().split(/ + /g);
     const command = args.shift().toLowerCase();
-
-    msg.channel.send(command);
 
     // Grab command from Enmap, check if invalid
     const cmd = client.commands.get(command);
