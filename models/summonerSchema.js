d31eee188c76c412a77e01182dbd027fb7e42e4f
chanhenry54 <hechan@ucsd.edu>
2019-08-23 22:25:32 -0700

    Renamed to summonerSchema

--- models/summonerSchema.js
+++ models/summonerSchema.js
@@ -1,11 +1,11 @@
 // Summoner model
 const mongoose = require('mongoose');
 
-const playerSchema = mongoose.Schema({
+const summonerSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     userID: String,
     region: String,
     summName: String
 });
 
-module.exports = mongoose.model('players', playerSchema);
+module.exports = mongoose.model('Summoner', summonerSchema);
