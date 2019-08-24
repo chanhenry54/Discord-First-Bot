8e0364b79699e7dae75102671481c032e3b5bf3c
chanhenry54 <hechan@ucsd.edu>
2019-08-23 22:24:09 -0700

    Redesigned to be more informative

--- commands/help.js
+++ commands/help.js
@@ -1,18 +1,22 @@
 // help command module
-exports.run = (client, message, args) => {
-    message.channel.send({
-        embed: {
-            "color": 8838087,
-            "fields": [
-                {
-                    "name": "Hechan Commands",
-                    "value": "Here is a list of all available Hechan commands and parameters."
-                },
-                {
-                    "name": "!hechan help",
-                    "value": "Displays this help guide"
-                }
-            ]
-        }
-    });
+module.exports = {
+    name: 'help',
+    description: 'Shows list of available commands',
+    run(client, message, args) {
+        message.channel.send({
+            embed: {
+                "color": 8838087,
+                "fields": [
+                    {
+                        "name": "Hechan Commands",
+                        "value": "Here is a list of all available Hechan commands and parameters."
+                    },
+                    {
+                        "name": "!hechan help",
+                        "value": "Displays this help guide"
+                    }
+                ]
+            }
+        });
+    }
 };
