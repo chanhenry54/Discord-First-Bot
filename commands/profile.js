b82f96db0570fe81a85be66805fa5fc3a282f2cc
chanhenry54 <hechan@ucsd.edu>
2019-08-28 18:32:24 -0700

    Refactored functions, match data obtained

--- commands/profile.js
+++ commands/profile.js
@@ -2,45 +2,27 @@
 const Discord = require('discord.js');
 const Summoner = require('../models/summonerSchema');
 
+// helper function to process match data
+const processMatch = async (championIdMap, summonerId, match) => {
+    const { participantId } = match.participantIdentities.find(pi => pi.player.summonerId === summonerId);
+    const participant = match.participants.find(p => p.participantId === participantId);
+    const champion = championIdMap.data[participant.championId];
+    return {
+        gameCreation: match.gameCreation,
+        didWin: participant.teamId === match.teams.find(({ win }) => win === 'Win').teamId,
+        championName: champion.name,
+        kills: participant.stats.kills,
+        deaths: participant.stats.deaths,
+        assists: participant.stats.assists
+    }
+}
+
 module.exports = {
     name: 'profile',
     description: 'Shows the summoner profile for a particular user',
     usage: '!hechan profile [region] [summoner]',
-    run(client, message, args, kayn, REGIONS) {
+    async run(client, message, args, kayn, REGIONS) {
         const numMatches = 10;
-        // helper function to output summoner profile
-        function outputProfile() {
-            kayn.Summoner.by.name(summonerName)
-                .region(region)
-                .then(summoner => {
-                    kayn.Matchlist.by
-                        .accountID(summoner.accountId)
-                        .region(region)
-                        .then(matchlist => {
-                            const matches = matchlist.matches.slice(0, numMatches);
-                            for (let i = 0; i < matches.length; i++) {
-
-                            }
-                            const embed = new Discord.RichEmbed()
-                                .setAuthor(`Summoner Profile: ${summonerName} [${region.toUpperCase()}]`)
-                                .setThumbnail(`https://opgg-static.akamaized.net/images/profile_icons/profileIcon${summoner.profileIconId}.jpg`)
-                                .setColor(0x86DBC7)
-                                .setDescription(`Here is some information about ${summonerName} [${region.toUpperCase()}].`)
-                                .addField('Level', summoner.summonerLevel, true);
-                            // last 20 games, top champs, ranked stats, last played
-                            return message.channel.send(embed);
-                        })
-                        .catch(err => {
-                            console.error(err);
-                            return message.channel.send('Oops, an error occurred! Please try again!');
-                        });
-                })
-                .catch(err => {
-                    console.error(err);
-                    return message.channel.send('Oops, an error occurred! Please try again!');
-                });
-        }
-
         let region;
         let summonerName;
         if (args.length > 0) { // if args are present
@@ -52,10 +34,9 @@ module.exports = {
                     return message.channel.send(`"${region}" is not a valid region. Valid regions are: ${Object.values(REGIONS).join(', ').toUpperCase()}`);
                 }
                 summonerName = args.join(' ');
-                outputProfile();
             }
         } else { // no args
-            Summoner.findOne({ userID: message.author.id }, function (err, result) {
+            await Summoner.findOne({ userID: message.author.id }).exec(function (err, result) {
                 if (err) {
                     console.error(err);
                     return message.channel.send('Oops, an error has occurred! Please try again!');
@@ -65,9 +46,29 @@ module.exports = {
                 } else {
                     region = result.region;
                     summonerName = result.summName;
-                    outputProfile();
                 }
             });
         }
+
+        // prep for getting match data
+        const championIdMap = await kayn.DDragon.Champion.listDataByIdWithParentAsId();
+        const { id, accountId, profileIconId, summonerLevel } = await kayn.Summoner.by.name(summonerName).region(region);
+        const { matches } = await kayn.Matchlist.by.accountID(accountId);
+        const gameIds = matches.slice(0, numMatches).map(({ gameId }) => gameId);
+        const games = await Promise.all(gameIds.map(kayn.Match.get));
+
+        // get match data
+        const processor = match => processMatch(championIdMap, id, match);
+        const results = await Promise.all(games.map(processor));
+
+        // output profile to Discord
+        const embed = new Discord.RichEmbed()
+            .setAuthor(`Summoner Profile: ${summonerName} [${region.toUpperCase()}]`)
+            .setThumbnail(`https://opgg-static.akamaized.net/images/profile_icons/profileIcon${profileIconId}.jpg`)
+            .setColor(0x86DBC7)
+            .setDescription(`Here is some information about ${summonerName} [${region.toUpperCase()}].`)
+            .addField('Level', summonerLevel, true);
+        // last 20 games, top champs, ranked stats, last played
+        return message.channel.send(embed);
     }
 };
